2d77fbaadb11a49ac27d5f93fac7fa9af8e48b03
Márleo Piber da Rosa <marleopr@hotmail.com>
2023-08-11 14:58:49 -0300

    Ajuste segurança api air pollution

--- src/constants/BASE_URL.js
+++ src/constants/BASE_URL.js
@@ -1,5 +1,5 @@
 // https://home.openweathermap.org/api_keys
 export const base_url = "https://api.openweathermap.org/data/2.5/weather?"
 export const base_url2 = "https://api.openweathermap.org/data/2.5/forecast?"
-export const base_urlAir = "http://api.openweathermap.org/data/2.5/air_pollution?"
+export const base_urlAir = "https://api.openweathermap.org/data/2.5/air_pollution?"
 export const apiKey = "4aa24fba0836345a6e5697132a1ba065"
